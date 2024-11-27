fdf0e1d2120ce3c450a94742a1a52199d0877fe4
zheglovd <d.zheglov@mail.utoronto.ca>
2024-11-26 22:10:18 -0500

    Added the Templates navbar

--- PP2/my-app/components/Navbar.tsx
+++ PP2/my-app/components/Navbar.tsx
@@ -30,6 +30,12 @@ export default function Navbar() {
         >
           Codespace
         </button>
+        <button
+          onClick={() => router.push('/settings')}
+          className="text-white hover:text-red-600"
+        >
+          Templates
+        </button>
       </div>
     </nav>
   );
